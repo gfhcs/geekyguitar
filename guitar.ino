4106fcb3124760c18cdca35e378d35efef491958
Gereon Fox <ger.fox07@t-online.de>
2014-06-02 21:42:50 +0200

    Made Arduino only send MIDI signals if the capacitive sensor is being used.

--- guitar.ino
+++ guitar.ino
@@ -7,6 +7,8 @@ int latchPin = 8;
 int dataPin = 9;
 int clockPin = 7;
 
+int csensorPin = 12;
+
 byte buttonStates = 255;  //01001000
 
 void setup() {
@@ -19,19 +21,19 @@ void setup() {
 
 }
 
-void loop() {
+const float csensor_threshold = 0.25f;
 
-  buttonStates = getButtonStates();
-  
-  //buttonStates = 255;
-  
-  byte notes[6];
-  
-  getNotes(buttonStates, notes);
-  
-  play(notes, 6);
-  
-  delay(1000);
+void loop() {
+  if (readCSensor() > csensor_threshold)
+  {
+    buttonStates = getButtonStates();
+    
+    byte notes[6];
+    
+    getNotes(buttonStates, notes);
+    
+    play(notes, 6);
+  }  
 }
 
 
@@ -154,9 +156,9 @@ void getNotes(byte data, byte notes[])
   sevth = (B00000001 & data) == B00000001;
 
   diff = (key - 'A') * 2;
-  if (diff > 10)
+  if (diff > 11)
     diff -= 2;
-  else if (diff > 4)
+  else if (diff > 3)
     diff--;
     
   notes[0] = 1 + diff;
@@ -184,4 +186,75 @@ void getNotes(byte data, byte notes[])
     notes[4] += tmp;
     notes[5] += tmp;
   }
+}
+
+float readCSensor()
+{
+  return (float)readCapacitivePin(csensorPin) / 17.0;
+}
+
+// readCapacitivePin
+//  Input: Arduino pin number
+//  Output: A number, from 0 to 17 expressing
+//  how much capacitance is on the pin
+//  When you touch the pin, or whatever you have
+//  attached to it, the number will get higher
+uint8_t readCapacitivePin(int pinToMeasure) {
+  // Variables used to translate from Arduino to AVR pin naming
+  volatile uint8_t* port;
+  volatile uint8_t* ddr;
+  volatile uint8_t* pin;
+  // Here we translate the input pin number from
+  //  Arduino pin number to the AVR PORT, PIN, DDR,
+  //  and which bit of those registers we care about.
+  byte bitmask;
+  port = portOutputRegister(digitalPinToPort(pinToMeasure));
+  ddr = portModeRegister(digitalPinToPort(pinToMeasure));
+  bitmask = digitalPinToBitMask(pinToMeasure);
+  pin = portInputRegister(digitalPinToPort(pinToMeasure));
+  // Discharge the pin first by setting it low and output
+  *port &= ~(bitmask);
+  *ddr  |= bitmask;
+  delay(1);
+  // Prevent the timer IRQ from disturbing our measurement
+  noInterrupts();
+  // Make the pin an input with the internal pull-up on
+  *ddr &= ~(bitmask);
+  *port |= bitmask;
+
+  // Now see how long the pin to get pulled up. This manual unrolling of the loop
+  // decreases the number of hardware cycles between each read of the pin,
+  // thus increasing sensitivity.
+  uint8_t cycles = 17;
+       if (*pin & bitmask) { cycles =  0;}
+  else if (*pin & bitmask) { cycles =  1;}
+  else if (*pin & bitmask) { cycles =  2;}
+  else if (*pin & bitmask) { cycles =  3;}
+  else if (*pin & bitmask) { cycles =  4;}
+  else if (*pin & bitmask) { cycles =  5;}
+  else if (*pin & bitmask) { cycles =  6;}
+  else if (*pin & bitmask) { cycles =  7;}
+  else if (*pin & bitmask) { cycles =  8;}
+  else if (*pin & bitmask) { cycles =  9;}
+  else if (*pin & bitmask) { cycles = 10;}
+  else if (*pin & bitmask) { cycles = 11;}
+  else if (*pin & bitmask) { cycles = 12;}
+  else if (*pin & bitmask) { cycles = 13;}
+  else if (*pin & bitmask) { cycles = 14;}
+  else if (*pin & bitmask) { cycles = 15;}
+  else if (*pin & bitmask) { cycles = 16;}
+
+  // End of timing-critical section
+  interrupts();
+
+  // Discharge the pin again by setting it low and output
+  //  It's important to leave the pins low if you want to 
+  //  be able to touch more than 1 sensor at a time - if
+  //  the sensor is left pulled high, when you touch
+  //  two sensors, your body will transfer the charge between
+  //  sensors.
+  *port &= ~(bitmask);
+  *ddr  |= bitmask;
+
+  return cycles;
 }
